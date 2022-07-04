186981873070dfc465db4421e587c02017918496
hibernator11 <gustavo.candela@cerrvantesvirtual.com>
2022-07-04 16:07:11 +0200

    onb shex

--- onb/onb_sparql_create_shex.py
+++ onb/onb_sparql_create_shex.py
@@ -26,7 +26,7 @@ namespaces_dict = {"http://www.w3.org/1999/02/22-rdf-syntax-ns#": "rdf",
                    "http://www.w3.org/2004/02/skos/core#": "skos",
                    "http://purl.org/dc/elements/1.1/": "dc",
                    "http://usefulinc.com/ns/doap": "doap",
-                   "http://rdfs.org/sioc/services#": "services"
+                   "http://rdfs.org/sioc/services#": "sioc"
                    }
 
 url_endpoint="https://lod.onb.ac.at/sparql/anno"
